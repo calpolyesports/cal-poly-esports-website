b46725d16c78dc50f8e5c5c7e2a1e49229c2efd9
EmuMan <jacob.kelleran@outlook.com>
2024-08-22 21:27:16 -0700

    Added Twitch and Twitter embeds

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,51 @@
 <script>
     import NewsFeed from './NewsFeed.svelte';
+    import TwitchEmbed from './TwitchEmbed.svelte';
+    import TwitterEmbed from './TwitterEmbed.svelte';
 
     export let data;
 </script>
 
-<div>
+<div class="container">
     <img src="/images/Banner.png" alt="Banner" />
 </div>
 
-<div>
+<div class="container">
+    <div class="embeds">
+        <div class="livestream">
+            <TwitchEmbed channel="CalPoly_Esports" />
+        </div>
+
+        <div class="twitter">
+            <TwitterEmbed channel="calpolyesports" refSrc="twsrc%5Etfw"/>
+        </div>
+    </div>
+</div>
+
+<div class="container">
     <NewsFeed articles={data.articles} />
 </div>
 
 <style>
-    div {
+    div.container {
         margin-top: 2rem;
         width: 100%;
+    }
+    
+    div.embeds {
+        display: flex;
+        justify-content: space-between;
+        height: 25rem;
+    }
+
+    div.livestream {
+        width: 54%;
+        height: 100%;
+    }
+
+    div.twitter {
+        width: 44%;
+        height: 100%;
     }
 
     img {
---
+++ src/routes/TwitchEmbed.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let channel;
+</script>
+
+<iframe
+    src="https://player.twitch.tv/?channel={channel}&parent=localhost&muted=true"
+    title="Twitch Embed"
+    height="100%"
+    width="100%"
+    allowfullscreen>
+</iframe>
