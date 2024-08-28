1a9c9da96b90fc84fae4267624da5872d0f88aca
EmuMan <jacob.kelleran@outlook.com>
2024-08-28 15:46:11 -0700

    Removed Twitter embed, added lab info box

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import NewsFeed from './NewsFeed.svelte';
     import TwitchEmbed from './TwitchEmbed.svelte';
-    import TwitterEmbed from './TwitterEmbed.svelte';
 
     import { PUBLIC_PAGE_URL } from '$env/static/public';
 
@@ -18,8 +17,10 @@
             <TwitchEmbed channel="CalPoly_Esports" parentUrl={PUBLIC_PAGE_URL} />
         </div>
 
-        <div class="twitter">
-            <TwitterEmbed channel="calpolyesports" refSrc="twsrc%5Etfw"/>
+        <div class="text-box">
+            <h1>Lab Information</h1>
+            <p>Come check out our new Esports and Gaming Lab, equipped with <strong>six high-end gaming PCs!</strong></p>
+            <p>The lab is currently located in <a href="https://maps.calpoly.edu/place/bldg-021-0">Building 21</a>, Room 233. You can find <a href="/calendar">open hours for free usage here.</a></p>
         </div>
     </div>
 </div>
@@ -44,13 +45,35 @@
         width: 54%;
         height: 100%;
     }
-
-    div.twitter {
+    
+    div.text-box {
         width: 44%;
         height: 100%;
+        outline: 3px solid var(--cal-poly-primary);
     }
 
     img {
         width: 100%;
+    }
+
+    h1 {
+        font-size: 3rem;
+        font-weight: bold;
+        margin: 1rem 2rem;
+        color: var(--cal-poly-secondary);
+        text-align: center;
+    }
+
+    p {
+        font-size: 1.5rem;
+        margin: 0.5rem 2rem;
+    }
+
+    a {
+        color: black;
+    }
+
+    strong {
+        color: var(--cal-poly-secondary);
     }
 </style>
