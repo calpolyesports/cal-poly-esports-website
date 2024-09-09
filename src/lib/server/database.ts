010ccbcc44f66d964944c0b404af160640e23156
Jacob Kelleran <jacob.kelleran@outlook.com>
2024-09-09 12:48:48 -0700

    Added roster-related database methods

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -70,14 +70,13 @@ export async function getRosterGameById(id: ObjectId) {
 export async function addRosterGame(game: InferRawDocType<typeof models.RosterGameModel>) {
     const newGame = new models.RosterGameModel(game);
     await newGame.save();
-    return newGame;
+    return newGame._id;
 }
 
 export async function updateRosterGame(id: ObjectId, game: InferRawDocType<typeof models.RosterGameModel>) {
-    const updatedGame = new models.RosterGameModel(game);
     await models.RosterGameModel.updateOne({
         _id: id,
-    }, updatedGame);
+    }, game);
 }
 
 export async function deleteRosterGame(id: ObjectId) {
@@ -94,6 +93,64 @@ export async function getRosterTeamById(id: ObjectId) {
     const teamDoc = await models.RosterTeamModel.findOne({ _id: id }).lean()
         .populate('members');
     return teamDoc;
+}
+
+export async function addRosterTeam(gameId: ObjectId, team: InferRawDocType<typeof models.RosterTeamModel>) {
+    const gameDoc = await models.RosterGameModel.findOne({ _id: gameId });
+    if (!gameDoc) return null;
+    const newTeam = new models.RosterTeamModel(team);
+    await newTeam.save();
+    gameDoc.teams.push(newTeam._id);
+    await gameDoc.save();
+    return newTeam._id;
+}
+
+export async function updateRosterTeam(id: ObjectId, team: InferRawDocType<typeof models.RosterTeamModel>) {
+    await models.RosterTeamModel.updateOne({
+        _id: id,
+    }, team);
+}
+
+export async function deleteRosterTeam(gameId: ObjectId, teamId: ObjectId) {
+    const gameDoc = await models.RosterGameModel.findOne({ _id: gameId });
+    if (!gameDoc) return;
+    await models.RosterTeamModel.deleteOne({ _id: teamId });
+    gameDoc.teams = gameDoc.teams.filter(team => team.toString() !== teamId.toString());
+    await gameDoc.save();
+}
+
+export async function getRosterMembers() {
+    const allMemberDocs = await models.RosterMemberModel.find().lean();
+    return allMemberDocs;
+}
+
+export async function getRosterMemberById(id: ObjectId) {
+    const memberDoc = await models.RosterMemberModel.findOne({ _id: id }).lean();
+    return memberDoc;
+}
+
+export async function addRosterMember(teamId: ObjectId, member: InferRawDocType<typeof models.RosterMemberModel>) {
+    const teamDoc = await models.RosterTeamModel.findOne({ _id: teamId });
+    if (!teamDoc) return null;
+    const newMember = new models.RosterMemberModel(member);
+    await newMember.save();
+    teamDoc.members.push(newMember._id);
+    await teamDoc.save();
+    return newMember._id;
+}
+
+export async function updateRosterMember(id: ObjectId, member: InferRawDocType<typeof models.RosterMemberModel>) {
+    await models.RosterMemberModel.updateOne({
+        _id: id,
+    }, member);
+}
+
+export async function deleteRosterMember(teamId: ObjectId, memberId: ObjectId) {
+    const teamDoc = await models.RosterTeamModel.findOne({ _id: teamId });
+    if (!teamDoc) return;
+    await models.RosterMemberModel.deleteOne({ _id: memberId });
+    teamDoc.members = teamDoc.members.filter(member => member.toString() !== memberId.toString());
+    await teamDoc.save();
 }
 
 export async function getArticles() {
