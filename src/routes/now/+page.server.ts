d77846732f6143ccc67595c1519535d53723a196
Jacob Kelleran <jacob.kelleran@outlook.com>
2024-09-27 15:48:43 -0700

    Added routes for social shortcuts

---
+++ src/routes/now/+page.server.ts
@@ -0,0 +1,7 @@
+import { redirect } from '@sveltejs/kit';
+
+import type { ServerLoad } from '@sveltejs/kit';
+
+export const load: ServerLoad = async (event) => {
+    redirect(302, "https://now.calpoly.edu/organization/esports");
+};
